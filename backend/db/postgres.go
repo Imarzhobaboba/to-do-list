e49f877a19eb45ba7b51823b65c25234d90e98c2
user <filia150@mail.ru>
2025-07-16 21:30:08 +0300

    one more time

--- backend/db/postgres.go
+++ backend/db/postgres.go
@@ -2,21 +2,26 @@ package db
 
 import (
 	"fmt"
+	"log"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitPostgres(host, user, password, dbname string, port int) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=UTC",
 		host, user, password, dbname, port,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info), // Включаем логирование SQL
+	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 
+	log.Println("Successfully connected to PostgreSQL!")
 	return db, nil
 }
